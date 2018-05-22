7d52d7dcfe38a585fcd8a446adc4334f5b9a7f4f
mattiabiondi <mattiabiondi1@gmail.com>
2018-05-22 19:35:20 +0200

    Wrong way. Deleted the Heatmap Provider. Added method for UMTS

--- app/src/main/java/biondi/mattia/signalstrengthheatmap/MainActivity.kt
+++ app/src/main/java/biondi/mattia/signalstrengthheatmap/MainActivity.kt
@@ -2,7 +2,6 @@ package biondi.mattia.signalstrengthheatmap
 
 import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.Color
 import android.location.Location
 import android.net.wifi.WifiManager
 import android.os.Bundle
@@ -13,14 +12,14 @@ import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.telephony.CellInfoWcdma
+import android.telephony.TelephonyManager
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.TileOverlay
-import com.google.android.gms.maps.model.TileOverlayOptions
 import com.google.maps.android.heatmaps.WeightedLatLng
 import kotlinx.android.synthetic.main.main_layout.*
 import kotlinx.android.synthetic.main.app_bar_layout.*
@@ -39,9 +38,11 @@ class MainActivity :
 
     // Codice di richiesta
     private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+    private val PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE = 2
 
     // Autorizzazioni
     private var locationPermission = false
+    private var networkPermission = false
 
     // Posizione attuale
     private var currentLocation: Location? = null
@@ -51,8 +52,8 @@ class MainActivity :
     private var locationRequest: LocationRequest? = null
 
     // Intervalli di tempo in cui si aggiorna la posizione
-    private val INTERVAL = 500L
-    private val FASTEST_INTERVAL = 500L
+    private val INTERVAL = 1000L
+    private val FASTEST_INTERVAL = 1000L
 
     // Comandi da eseguire dopo aver ottenuto la posizione
     private lateinit var locationCallback: LocationCallback
@@ -89,15 +90,11 @@ class MainActivity :
     private var wifiItem: MenuItem? = null
 
     // Lista delle coordinate ottenute dal dispositivo
-    private var wifiList = mutableListOf<WeightedLatLng>()
+    var umtsList = mutableListOf<WeightedLatLng>()
+    var lteList = mutableListOf<WeightedLatLng>()
+    var wifiList = mutableListOf<WeightedLatLng>()
 
-    // Istanza del HeatmapTileProvider
-    private var wifiProvider: WeightedHeatmapTileProvider? = null
-
-    // Istanza del tile wifiOverlay
-    private var wifiOverlay: TileOverlay? = null
-
-    private val WIFI_PRECISION = 10
+    val PRECISION = 5
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -164,7 +161,7 @@ class MainActivity :
         wifiBoolean = savedInstanceState.getBoolean(WIFI_BOOLEAN_KEY)
     }
 
-    private fun getLocationPermission () {
+    private fun getLocationPermission() {
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // Permessi non ottenuti
             // Fornisce una spiegazione all'utente se quest'ultimo nega più volte i permessi per accedere alla posizione
@@ -180,6 +177,22 @@ class MainActivity :
         }
     }
 
+    private fun getWcdmaPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // Permessi non ottenuti
+            // Fornisce una spiegazione all'utente se quest'ultimo nega più volte i permessi per accedere alla posizione
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_NETWORK_STATE)) {
+                Toast.makeText(this, "Network permission needed.", Toast.LENGTH_SHORT).show()
+            } else {
+                // Nessuna spiegazione necessaria, possiamo chiedere i permessi
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_NETWORK_STATE), PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE)
+            }
+        } else {
+            // Permessi già ottenuti
+            networkPermission = true
+        }
+    }
+
     // Si occupa del risultato delle richieste
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         when(requestCode) {
@@ -192,6 +205,17 @@ class MainActivity :
                 }
                 return
             }
+
+            PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE -> {
+                // Se la richiesta viene cancellata gli array risultanti sono vuoti
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permessi ottenuti
+                } else {
+                    // Permessi negati
+                }
+                return
+            }
+
         }
     }
 
@@ -238,6 +262,7 @@ class MainActivity :
                     // Aggiorna la posizione attuale
                     previousLocation = currentLocation
                     currentLocation = location
+                    textLatLng.text = location.latitude.toString() + ", " + location.longitude.toString()
                     // Passa la posizione attuale alla funzione che si occupa di generare la Heatmap
                     if (startBoolean) checkLocation()
                 }
@@ -335,8 +360,6 @@ class MainActivity :
                     invalidateOptionsMenu()
                     // Svuota la lista
                     wifiList.clear()
-                    wifiOverlay?.remove()
-                    wifiProvider = null
                 })
                 alert.setNegativeButton(R.string.alert_dialog_negative, {
                     _, _ ->  //niente
@@ -355,6 +378,7 @@ class MainActivity :
                 umtsItem = item
                 umtsBoolean = !umtsBoolean
                 umts_switch.isChecked = umtsBoolean
+                getWcdmaPermission()
             }
             R.id.lte_item -> {
                 lteItem = item
@@ -365,7 +389,6 @@ class MainActivity :
                 wifiItem = item
                 wifiBoolean = !wifiBoolean
                 wifi_switch.isChecked = wifiBoolean
-                wifiOverlay?.isVisible = wifiBoolean
             }
             R.id.settings -> {
                 // TODO: implementa impostazioni
@@ -377,45 +400,46 @@ class MainActivity :
         return true
     }
 
-    private fun wifiHeatMap(location: Location) {
-        if (wifiBoolean) {
-            val wifiRadius = 30
-            val wifiOpacity = 1.0
-            val wifiLatLng = getWifiWeight(location)
-            val wifiGradient = getGradient()
-            // Aggiunge la posizione attuale alla lista
-            wifiList.add(wifiLatLng)
-            // Controlla se bisogna inizializzare il wifiProvider
-            if (wifiProvider == null) {
-                // Inizializza il wifiProvider, passandogli i dati presenti in lista (nessuno al momento della creazione)
-                wifiProvider = WeightedHeatmapTileProvider(wifiList)
-                wifiProvider?.setRadius(wifiRadius)
-                wifiProvider?.setGradient(wifiGradient)
-                wifiProvider?.setOpacity(wifiOpacity)
-                wifiProvider?.setMaxIntensity(WIFI_PRECISION)
-                // Aggiunge l'overlay alla mappa, utilizzando il wifiProvider
-                wifiOverlay = map?.addTileOverlay(TileOverlayOptions().fadeIn(false).tileProvider(wifiProvider))
-            } else {
-                // Modifica i dati del wifiProvider
-                wifiProvider?.setWeightedData(wifiList)
-                // Forza un ricaricamento dei punti sulla mappa
-                wifiOverlay?.clearTileCache()
+    private fun checkLocation() {
+        if (currentLocation != previousLocation) { //todo da migliorare
+            if (wifiBoolean) {
+                addUmtsLocation(currentLocation!!)
+                addWifiLocation(currentLocation!!)
             }
         }
     }
 
-    private fun getGradient() : biondi.mattia.signalstrengthheatmap.Gradient {
-        // I colori da utilizzare nella mappa
-        val colors = intArrayOf(
-                Color.GREEN,
-                Color.YELLOW,
-                Color.RED)
-        // Il valore di inizio di ogni colore
-        // Avendo definito 4 colori specifici non c'è nessuna vera transizione tra loro
-        val startPoints = floatArrayOf(0.33F, 0.66F, 1.0F)
-        // La "risoluzione" del punto che viene disegnato
-        val colorMapSize = 1000
-        return biondi.mattia.signalstrengthheatmap.Gradient(colors, startPoints, colorMapSize)
+    private fun addUmtsLocation(location: Location) {
+        val latLng = getUmtsWeight(location)
+        // Aggiunge la posizione attuale alla lista
+        umtsList.add(latLng)
+    }
+    private fun addWifiLocation(location: Location) {
+        val latLng = getWifiWeight(location)
+        // Aggiunge la posizione attuale alla lista
+        wifiList.add(latLng)
+
+    }
+
+    private fun getUmtsWeight(location: Location) : WeightedLatLng {
+        var intensity = WeightedLatLng.DEFAULT_INTENSITY
+        val telephonyManager = applicationContext.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+
+       //@TargetApi(Build.VERSION_CODES.O)
+       // if (!telephonyManager.isDataEnabled) Toast.makeText(this, "Turn on data", Toast.LENGTH_SHORT).show()
+
+        try {
+            if (networkPermission) {
+                val cellList = telephonyManager.allCellInfo
+                if (cellList != null && cellList.isNotEmpty()){
+                    val cellInfoWcdma = telephonyManager.allCellInfo[0] as CellInfoWcdma //todo android.telephony.CellInfoLte cannot be cast to android.telephony.CellInfoWcdma
+                    intensity = cellInfoWcdma.cellSignalStrength.level.toDouble()
+                    textUMTS1.text = (intensity + 1).toInt().toString()
+                }
+            }
+        } catch (e: SecurityException) {
+        }
+        return WeightedLatLng(LatLng(location.latitude, location.longitude), intensity)
     }
 
     private fun getWifiWeight(location: Location) : WeightedLatLng {
@@ -424,16 +448,12 @@ class MainActivity :
         if (wifiManager.isWifiEnabled) {
             val wifiInfo = wifiManager.connectionInfo
             if (wifiInfo != null) {
-                intensity = WifiManager.calculateSignalLevel(wifiInfo.rssi, WIFI_PRECISION).toDouble()
-                textWifi.text = intensity.toString()
+                intensity = WifiManager.calculateSignalLevel(wifiInfo.rssi, PRECISION).toDouble()
+                textWifi1.text = (intensity + 1).toInt().toString()
             }
         }
         return WeightedLatLng(LatLng(location.latitude, location.longitude), intensity)
     }
-
-    private fun checkLocation() {
-        if (currentLocation != previousLocation) {
-            if (wifiBoolean) wifiHeatMap(currentLocation as Location)
-        }
-    }
 }
+
+//todo controlli is enabled sugli interruttori, mostrare la mappa solo dopo aver ottenuto i permessi
